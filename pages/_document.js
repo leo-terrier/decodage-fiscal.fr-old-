b70d3f48e39807290f3c06c4423fa340d33433b2
Leo Terrier <leoterrier22@gmail.com>
2022-10-31 22:47:42 +0100

    version 1 = done

--- pages/_document.js
+++ pages/_document.js
@@ -1,25 +1,30 @@
 import { Head, Html, Main, NextScript } from 'next/document';
+import { useEffect } from 'react';
 
 import { hotjar } from 'react-hotjar';
 
-hotjar.initialize('3226816', '6');
 
-// Identify the user
-hotjar.identify('USER_ID', { userProperty: 'value' });
+useEffect(() => {
+  hotjar.initialize('3226816', '6');
 
-// Identify the user
-hotjar.identify('USER_ID', { userProperty: 'value' });
-
-// Add an event
-hotjar.event('button-click');
-
-// Update SPA state
-hotjar.stateChange('/my/page');
-
-// Check if Hotjar has been initialized before calling its methods
-if (hotjar.initialized()) {
+  // Identify the user
   hotjar.identify('USER_ID', { userProperty: 'value' });
-}
+
+  // Identify the user
+  hotjar.identify('USER_ID', { userProperty: 'value' });
+
+  // Add an event
+  hotjar.event('button-click');
+
+  // Update SPA state
+  hotjar.stateChange('/my/page');
+
+  // Check if Hotjar has been initialized before calling its methods
+  if (hotjar.initialized()) {
+    hotjar.identify('USER_ID', { userProperty: 'value' });
+  }
+}, [])
+
 
 export default function Document() {
   return (
